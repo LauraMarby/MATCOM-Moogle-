2a216548cee28fd9342b3f9a41e885423752ee08
LauraMarby <lauramartir2@gmail.com>
2023-07-24 22:50:59 -0400

    Segunda Actualización de Prácticas Laborales

--- Script/proyecto.sh
+++ Script/proyecto.sh
@@ -6,8 +6,8 @@ if [ $1 == "run" ]; then
 elif [ $1 == "clean" ]; then
     cd ../Informe/
     find . ! -name Informe.tex -type f -delete
-    cd ../Presentacion/
-    find . ! -name Presentacion.tex -type f -delete
+    cd ../Presentación/
+    find . ! -name Presentación.tex -type f -delete
     echo "Los ficheros de compilación auxiliar de los PDF han sido eliminados satisfactoriamente"
     cd ../MoogleEngine
     rm -r bin
@@ -20,32 +20,36 @@ elif [ $1 == "report" ]; then
     cd ../Informe/
     pdflatex Informe
 elif [ $1 == "slides" ]; then
-    cd ../Presentacion/
-    pdflatex Presentacion
+    cd ../Presentación/
+    pdflatex Presentación
 elif [ $1 == "show_report" ]; then
     cd ../Informe/
     if [ ! -f Informe.pdf ]; then
         echo Creando Informe...
         pdflatex Informe
         echo Abriendo Informe...
-
     else
         echo Abriendo Informe...
-
     fi
-    evince Informe.pdf
+    if [ -z "$2" ]; then
+        evince Informe.pdf
+    else
+        $2 Informe.pdf
+    fi
 elif [ $1 == "show_slides" ]; then
-    cd ../Presentacion/
-    if [ ! -f Presentacion.pdf ]; then
+    cd ../Presentación/
+    if [ ! -f Presentación.pdf ]; then
         echo Creando Presentación...
-        pdflatex Presentacion
+        pdflatex Presentación
         echo Abriendo Presentación...
-
     else
         echo Abriendo Presentación...
-
     fi
-    evince Presentacion.pdf
+    if [ -z "$2" ]; then
+        evince Presentación.pdf
+    else
+        $2 Presentación.pdf
+    fi
 else
-    echo "Comando inválido. Por favor reinicie el programa e introduzca un comando válido"
+    echo "Comando inválido"
 fi
